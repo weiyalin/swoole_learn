1066d2a3361e220c48f1b6ae944c7d8adb4ac35b
weiyalin <727299708@qq.com>
2019-07-30 19:40:17 +0800

    '测试'

--- Server.php
+++ Server.php
@@ -5,48 +5,53 @@
  * Created by Wilin
  */
 
-class Server
-{
-    private $serv;
+class Server {
+    private $server;
+    private $table;
 
-    public function __construct()
-    {
-        $this->serv = new swoole_server("0.0.0.0",9501);
-        $this->serv->set(array(
-           'worker_num' => 8,
-           'daemonize' => false
-        ));
+    public function __construct() {
 
-        $this->serv->on('Start', array($this,'onStart'));
-        $this->serv->on('Connect', array($this,'onConnect'));
-        $this->serv->on('Receive', array($this,'onReceive'));
-        $this->serv->on('Close', array($this,'onClose'));
+        $this->server = new swoole_websocket_server("0.0.0.0",9501);
+        $this->server->on('open', array($this,'onOpen'));
+        $this->server->on('message', array($this,'onMessage'));
+        $this->server->on('close', array($this,'onClose'));
 
-        $this->serv->start();
-
+        $this->createTable();
+        $this->server->start();
     }
 
-    public function onStart($serv)
-    {
-        echo "Start\n";
+    public function onOpen(\Swoole\WebSocket\Server $server, $request) {
+        echo $request->fd . '连接了' . PHP_EOL;
+        $user = [
+            'fd' => $request->fd,
+            'name' => '用户' . $request->fd,
+        ];
+
+        $this->table->set($request->fd, $user);
     }
 
-    public function onConnect(swoole_server $serv, $fd, $from_id)
-    {
-        $serv->send($fd, "Hello {$fd}");
+    public function onMessage(\Swoole\WebSocket\Server $server, $frame) {
+        $name = $this->table->get($frame->fd)['name'];
+        echo $name . '发来消息：' . $frame->data . PHP_EOL;
+        foreach ($this->table as $user) {
+            if ($user['fd'] == $frame->fd) {
+                continue;
+            }
+            $server->push($user['fd'], json_encode(['username' => $name, 'message' => $frame->data ]));
+        }
     }
 
-    public function onReceive(swoole_server $serv, $fd, $from_id, $data)
-    {
-        echo "Get Message From Client {$fd}:{$data}\n";
-        $serv->send($fd, $data);
+    public function onClose($server, $fd) {
+        $name = $this->table->get($fd)['name'];
+        echo $name . '断开连接，内存表删除：' . $this->table->del($fd) . PHP_EOL;
     }
 
-    public function onClose($serv, $fd, $from_id)
-    {
-        echo "Client {$fd} close connection\n";
+    private function createTable() {
+        $this->table = new \Swoole\Table(1024);
+        $this->table->column('fd', \Swoole\Table::TYPE_INT);
+        $this->table->column('name', \Swoole\Table::TYPE_STRING);
+        $this->table->create();
     }
-
 }
 
 $server = new Server();
